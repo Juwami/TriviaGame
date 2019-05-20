4ce1ee140333293d17195848d42feba0fae2c589
Juwami <sean.nguyenf@yahoo.com>
2019-05-19 23:26:35 -0400

    added functions for countdown to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+let time = 30
+
+window.onload = function () {
+    $('.timer').text('00:30')
+    start()
+}
+
+
+
+function start() {
+        intervalId = setInterval(count, 1000)
+}
+
+function endGame() {
+    $('.triviaQuestions').empty()
+
+}
+
+function timeConverter(t) {
+    let minutes = Math.floor(t / 60)
+    let seconds = t - (minutes * 60)
+
+    if (seconds < 10) {
+        seconds = '0' + seconds
+    }
+
+    if (minutes === 0) {
+        minutes = '00'
+    } else if (minutes < 10) {
+        minutes = '0' + minutes
+    }
+
+    return minutes + ':' + seconds
+}
+
+function count() {
+    time--
+    let converted = timeConverter(time)
+    $('.timer').text(converted)
+    console.log(time)
+
+    if (time === 0) {
+        endGame()
+        clearInterval()
+    }
+}
